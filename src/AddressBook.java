6f7c4838bbaae7a6887ca6f4c8ad4d4fad580ad4
Hamna Nimra <38963550+HamnaNimra@users.noreply.github.com>
2018-09-18 11:53:30 -0400

    Added a output statement mentioning it's an Address Book.

---
+++ src/AddressBook.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+	
+	private List<BuddyInfo> buddyInfo;
+	
+	public AddressBook() {
+		this.buddyInfo= new ArrayList<BuddyInfo>();
+	}
+	
+	public void addBuddy(BuddyInfo Buddy){
+		if (Buddy != null){
+			this.buddyInfo.add(Buddy);
+		}
+	}
+	
+	public BuddyInfo removeBuddy(int index){
+		if(index >= 0 && index < this.buddyInfo.size()){
+			return this.buddyInfo.remove(index);
+		}
+		return null;
+	}
+	
+}
